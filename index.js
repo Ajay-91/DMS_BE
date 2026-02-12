664775761ac0ce4d13cc9c17b0eab497fd83483f
Ajay-91 <ajay0m911@gmail.com>
2026-02-12 21:38:20 +0530

    Fix: Use PORT environment variable for Render deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const router = require("./routes");
 
 const server = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.use(cors());
 
 //parser middleware
